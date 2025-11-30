92ee62ee16489f9af38d418821a6225c8896295c
Tony-MCL <MorningCoffeeLabs@gmail.com>
2025-11-30 16:22:32 +0100

    Update license.tsx

--- lib/license.tsx
+++ lib/license.tsx
@@ -49,7 +49,8 @@ type FirestoreDocument = {
 };
 
 const FIRESTORE_PROJECT_ID =
-  process.env.NEXT_PUBLIC_FIRESTORE_PROJECT_ID || "";
+  process.env.NEXT_PUBLIC_FIRESTORE_PROJECT_ID ||
+  process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "";
 const FIREBASE_API_KEY = process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "";
 
 /** Hjelper for å lese felter trygt */
