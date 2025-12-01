4448623052cdefd744c677a6cc525a57d42386ea
Tony-MCL <MorningCoffeeLabs@gmail.com>
2025-12-01 11:45:54 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "../app/globals.css";
 import "katex/dist/katex.min.css";
 import { I18nProvider } from "../lib/i18n";
 import { LicenseProvider } from "../lib/license";
+import LicenseDebug from "../components/LicenseDebug";
 
 export const metadata: Metadata = {
   title: "Digital Formelsamling – Morning Coffee Labs",
@@ -19,7 +20,10 @@ export default function RootLayout({
     <html lang="no" suppressHydrationWarning>
       <body>
         <I18nProvider>
-          <LicenseProvider>{children}</LicenseProvider>
+          <LicenseProvider>
+            {children}
+            <LicenseDebug />
+          </LicenseProvider>
         </I18nProvider>
       </body>
     </html>
